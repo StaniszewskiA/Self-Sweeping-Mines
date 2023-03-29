fe654ff9ff142a4c6aff9dfe801f748d646e98a3
AStaniszewski <staniszewski01adam@gmail.com>
2023-03-29 20:41:59 +0200

    Gameboard generates just bombs

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-import pygame
+import random
+
+def print_board(board_size, bombs):
+    bomb_positions = set()
+    while len(bomb_positions) < bombs:
+        pos = (random.randint(1, board_size), random.randint(1, board_size))
+        bomb_positions.add(pos)
+
+    for row in range(1, board_size + 1):
+        for col in range(1, board_size + 1):
+            if (row, col) in bomb_positions:
+                print("B", end="\t")
+            else:
+                print(" ", end="\t")
+        print()
+
+board_size = 9
+bombs = 10
 
 def main():
-    pass
+    print_board(board_size, bombs)
 
 if __name__ == "__main__":
     main()
