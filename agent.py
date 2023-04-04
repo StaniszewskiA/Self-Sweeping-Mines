ac6fdbdd3788d55b7995ea3b8c719af88a9cf515
AStaniszewski <staniszewski01adam@gmail.com>
2023-04-04 20:29:12 +0200

    Q list made with dictionary comprehension; _choose_action implemented

--- agent.py
+++ agent.py
@@ -11,18 +11,27 @@ class QLearningAgent:
         self.alpha = alpha
         self.gamma = gamma
         self.actions = actions
+        self.q_table = self._init_q_table()
 
     def _init_q_table(self):
-        q_table = {}
-        for i in range(self.board_size):
-            for j in range(self.board_size):
-                for action in self.actions:
-                    q_table[((i,j), action)] = 0
+        #q_table made with dictionary comprehension
+        q_table = {((i,j), action): 0 for i in range(self.board_size)
+                                        for j in range(self.board_size)
+                                        for action in self.actions}
         print(len(q_table))
         return q_table
 
     def _choose_action(self, state):
-        pass
+        #Explore with propability epsilon
+        if random.random() < self.epsilon:
+            return random.choice(self.actions)
+        #Otherwise, use the Q-table
+        else:
+            state_actions = [((state[0], state[1]), a) for a in self.actions]
+            q_values = [self.q_table.get(sa, 0) for sa in state_actions]
+            max_q_value = max(q_values)
+            max_actions = [a for a, q in zip(self.actions, q_values) if q == max_q_value]
+            return random.choice(max_actions)
 
     def _update_q_table(self, state, action, reward, next_state):
         pass
@@ -39,10 +48,16 @@ def main():
     bombs = 10
     actions = [(i, j, 'R') for i in range(board_size) for j in range(board_size)] + \
               [(i, j, 'F') for i in range(board_size) for j in range(board_size)]
+
     agent = QLearningAgent(board_size, bombs, actions)
+    """
     agent.train(episodes=10000)
     agent.test(episodes=3)
     agent._init_q_table()
+    """
+    state = ((0,0), 'R')
+    action = agent._choose_action(state)
+    print(action)
 
 if __name__ == "__main__":
     main()
